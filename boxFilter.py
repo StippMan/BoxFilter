7628558c95cacae0419323b37647bbff54de1aa9
StippMan <ra102919@uem.br>
2020-10-28 22:06:34 -0300

    arrumei o modo em que as imagens são mostradas após a execução

--- boxFilter.py
+++ boxFilter.py
@@ -1,22 +1,14 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import skimage as ski
 import sys
 import getopt as gpt
 
-def viewImages(images, names_of_window):
-
-	for i, name in enumerate(names_of_window):
-		cv.namedWindow(name, cv.WINDOW_NORMAL)
-		cv.imshow(name, images[i])
-
-	cv.waitKey(0)
-	cv.destroyAllWindows()
 
 imgfile_in = 'peppers.png'
 boxradius = 1
-
 reduction_rate = 2
+
 
 args = sys.argv
 for i, arg in enumerate(args):
@@ -27,7 +19,7 @@ for i, arg in enumerate(args):
 
 imgfile_out = imgfile_in
 
-input_img = cv.imread("input_images/"+imgfile_in,1)
+input_img = cv2.imread("input_images/"+imgfile_in,1)
 boxsize = boxradius*2 + 1
 box = np.ones((boxsize,boxsize))
 
@@ -36,9 +28,20 @@ box = np.ones((boxsize,boxsize))
 downsampled_img = input_img
 blurred_img = downsampled_img
 
-cv.imwrite("output_images/"+imgfile_out,img_out)
-cv.imwrite("output_images/"+imgfile_out,img_out)
+cv2.imwrite("output_images/"+imgfile_out,downsampled_img)
+cv2.imwrite("output_images/"+imgfile_out,blurred_img)
 
-viewImages([input_img, img_out], [imgfile_in,imgfile_out])
+cv2.namedWindow('input', cv2.WINDOW_NORMAL)
+cv2.imshow("input", input_img)
+
+cv2.namedWindow('downsampled', cv2.WINDOW_NORMAL)
+cv2.imshow("downsampled", downsampled_img)
+
+cv2.namedWindow('blurred', cv2.WINDOW_NORMAL)
+cv2.imshow("blurred", blurred_img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
 # viewImage(img_out,imgfile_out)
 
