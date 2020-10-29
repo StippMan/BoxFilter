d62443e4425a4b2d694a91c083fd44358d68c1ba
StippMan <ra102919@uem.br>
2020-10-28 22:33:56 -0300

    começo do desenvolvimento do box_blur

--- boxFilter.py
+++ boxFilter.py
@@ -4,11 +4,20 @@ import skimage as ski
 import sys
 import getopt as gpt
 
+def box_blur(img,box_size):
+	"""
+	Applies a box_blur of size box_size to img
+
+	Returns an image of the same dimensions as img
+	"""
+
+	blurred_img = img[:]
+
+	return blurred_img
+
 
 input_imgfile = 'peppers.png'
-boxradius = 1
 reduction_rate = 2
-
 
 args = sys.argv
 for i, arg in enumerate(args):
@@ -16,32 +25,30 @@ for i, arg in enumerate(args):
 		input_imgfile = args[i + 1]
 	elif arg == "-r":
 		reduction_rate = int(args[i + 1])
-
-
+	elif arg == "-b":
+		box_size = int(args[i + 1])
 
 input_img = cv2.imread("input_images/"+input_imgfile,1)
-boxsize = boxradius*2 + 1
-box = np.ones((boxsize,boxsize))
+box = np.ones((box_size,box_size))
 
 
 imgfile_name,imgfile_extension = input_imgfile.split('.')
 
-downsampled_img = input_img
+downsampled_img = input_img[::reduction_rate,::reduction_rate]
 downsampled_imgfile = '.'.join([imgfile_name+'_downsampled',imgfile_extension])
 
-blurred_img = downsampled_img
+blurred_img = box_blur(downsampled_img,box_size)
 blurred_imgfile = '.'.join([imgfile_name+'_blurred',imgfile_extension])
 
 cv2.imwrite("output_images/"+downsampled_imgfile,downsampled_img)
 cv2.imwrite("output_images/"+blurred_imgfile,blurred_img)
 
 cv2.namedWindow('input_imgfile', cv2.WINDOW_NORMAL)
-cv2.imshow("input_imgfile", input_img)
-
 cv2.namedWindow('downsampled_imgfile', cv2.WINDOW_NORMAL)
-cv2.imshow("downsampled_imgfile", downsampled_img)
-
 cv2.namedWindow('blurred_imgfile', cv2.WINDOW_NORMAL)
+
+cv2.imshow("input_imgfile", input_img)
+cv2.imshow("downsampled_imgfile", downsampled_img)
 cv2.imshow("blurred_imgfile", blurred_img)
 
 cv2.waitKey()
